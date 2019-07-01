a5f48a22169af68e30fc13ac5cd500e3282bb24a
eunsoopark <eunsoo.park@icon.foundation>
2019-07-01 15:29:32 +0900

    Added a sample which makes a new account for test

--- test-suite/test_sample.py
+++ test-suite/test_sample.py
@@ -1,10 +1,11 @@
 import os
 
-from iconsdk.builder.transaction_builder import CallTransactionBuilder
+from iconsdk.builder.transaction_builder import CallTransactionBuilder, TransactionBuilder
 from iconsdk.builder.call_builder import CallBuilder
 from iconsdk.icon_service import IconService
 from iconsdk.providers.http_provider import HTTPProvider
 from iconsdk.signed_transaction import SignedTransaction
+from iconsdk.wallet.wallet import KeyWallet
 
 from tbears.libs.icon_integrate_test import IconIntegrateTestBase
 
@@ -22,11 +23,39 @@ class TestScoreTest(IconIntegrateTestBase):
         # if you want to send request to network, uncomment next line and set self.TEST_HTTP_ENDPOINT_URI_V3
         self.icon_service = IconService(HTTPProvider(self.TEST_HTTP_ENDPOINT_URI_V3))
 
-    def test_stake(self):
-        stake_value: int = 0x64
-        account = self._wallet_array[0]
+    def _make_account(self, balance: int = 1000) -> 'KeyWallet':
+        # create account
+        account: 'KeyWallet' = KeyWallet.create()
 
-        # Generates a 'setStake' instance of transaction for calling method in SCORE.
+        # Generates an instance of transaction for sending icx.
+        transaction = TransactionBuilder() \
+            .from_(self._test1.get_address()) \
+            .to(account.get_address()) \
+            .value(balance) \
+            .step_limit(1000000) \
+            .nid(3) \
+            .nonce(100) \
+            .build()
+
+        # Returns the signed transaction object having a signature
+        signed_transaction = SignedTransaction(transaction, self._test1)
+
+        # process the transaction
+        tx_result = self.process_transaction(signed_transaction, self.icon_service)
+
+        self.assertTrue('status' in tx_result)
+        self.assertEqual(1, tx_result['status'])
+
+        return account
+
+    def test_stake(self):
+        # use pre-defined account
+        # account = self._wallet_array[0]
+        # make new account
+        account = self._make_account()
+        stake_value: int = 0x64
+
+        # Generates a 'setStake' instance of transaction
         transaction = CallTransactionBuilder() \
             .from_(account.get_address()) \
             .to(self.SYSTEM_ADDRESS) \
@@ -40,7 +69,7 @@ class TestScoreTest(IconIntegrateTestBase):
         # Returns the signed transaction object having a signature
         signed_transaction = SignedTransaction(transaction, account)
 
-        # process the transaction in local
+        # process the transaction
         tx_result = self.process_transaction(signed_transaction, self.icon_service)
 
         self.assertTrue('status' in tx_result)
