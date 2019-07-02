a371728f48b994eee35b9bdee997047888d70b93
leeheonseung <mycom22@icon.foundation>
2019-07-02 21:08:48 +0900

    add delegate test

--- test-suite/json-rpc-api/test_delegation.py
+++ test-suite/json-rpc-api/test_delegation.py
@@ -51,7 +51,6 @@ class TestDelegation(IconIntegrateTestBase):
     def _make_account_bulk(self, accounts: list, balance: int = 1000, count: int = 100) -> None:
 
         tx_list: list = []
-        tx_results: list = []
         for i in range(count):
             # create account
             account: 'KeyWallet' = KeyWallet.create()
@@ -79,8 +78,11 @@ class TestDelegation(IconIntegrateTestBase):
             self.assertEqual(1, tx['status'])
 
     def _get_block_height(self) -> int:
-        block = self.icon_service.get_block("latest")
-        return block['height']
+        block_height: int = 0
+        if self.icon_service:
+            block = self.icon_service.get_block("latest")
+            block_height = block['height']
+        return block_height
 
     def _init_prep(self, accounts: list):
         tx_list: list = []
@@ -95,7 +97,7 @@ class TestDelegation(IconIntegrateTestBase):
             }
 
             transaction = CallTransactionBuilder() \
-                .from_(accounts[i].get_address()) \
+                .from_(account.get_address()) \
                 .to(self.SYSTEM_ADDRESS) \
                 .step_limit(10_000_000) \
                 .nid(3) \
@@ -103,39 +105,18 @@ class TestDelegation(IconIntegrateTestBase):
                 .method("registerPRep") \
                 .params(info) \
                 .build()
-            signed_transaction = SignedTransaction(transaction, accounts[i])
+            signed_transaction = SignedTransaction(transaction, account)
             tx_list.append(signed_transaction)
         tx_results = self.process_transaction_bulk(tx_list, self.icon_service)
         for tx_result in tx_results:
             self.assertTrue('status' in tx_result)
             self.assertEqual(1, tx_result['status'])
 
-    def test_delegation6(self):
-        init_balance: int = 100
-        init_account_count: int = 100
-        init_block_height: int = self._get_block_height()
-
-        accounts: list = []
-        self._make_account_bulk(accounts, balance=init_balance, count=init_account_count)
-
-        # register prep
-        self._init_prep(accounts)
-
-    def test_delegation7(self):
-        init_balance: int = 100
-        init_account_count: int = 100
-        init_block_height: int = self._get_block_height()
-
-        accounts: list = []
-        self._make_account_bulk(accounts, balance=init_balance, count=init_account_count)
-
-        # register prep
-        self._init_prep(accounts)
-
+    def _set_stake_bulk(self, accounts: list, get_stake: callable):
         # set stake
         tx_list: list = []
         for i, account in enumerate(accounts):
-            stake_value: int = init_balance - i % 100 // 2
+            stake_value: int = get_stake(i)
             transaction = CallTransactionBuilder() \
                 .from_(accounts[i].get_address()) \
                 .to(self.SYSTEM_ADDRESS) \
@@ -152,21 +133,332 @@ class TestDelegation(IconIntegrateTestBase):
             self.assertTrue('status' in tx_result)
             self.assertEqual(1, tx_result['status'])
 
-        # get stake
-        for i in range(init_account_count):
-            stake_value: int = init_balance - i % 100 // 2
-            call = CallBuilder().from_(accounts[i].get_address()) \
+    def _get_stake_bulk(self, accounts: list, get_stake: callable):
+        for i, account in enumerate(accounts):
+            stake_value: int = get_stake(i)
+            call = CallBuilder().from_(account.get_address()) \
                 .to(self.SYSTEM_ADDRESS) \
                 .method("getStake") \
-                .params({"address": accounts[i].get_address()}) \
+                .params({"address": account.get_address()}) \
                 .build()
             response = self.process_call(call, self.icon_service)
             expected_result = {
-                "stake": hex(stake_value),
-                "unstake": "0x0",
-                "unstakedBlockHeight": "0x0"
+                "stake": hex(stake_value)
             }
             self.assertEqual(expected_result, response)
+
+    def _set_delegate(self,
+                      account: 'KeyWallet',
+                      accounts: list,
+                      cnt: int,
+                      start_index: int,
+                      delegation_value: int):
+        delegations: list = []
+        if delegation_value > 0:
+            for i in range(start_index, start_index + cnt):
+                delegation: dict = {
+                    "address": str(accounts[i].get_address()),
+                    "value": hex(delegation_value)
+                }
+                delegations.append(delegation)
+        transaction = CallTransactionBuilder() \
+            .from_(account.get_address()) \
+            .to(self.SYSTEM_ADDRESS) \
+            .step_limit(10_000_000) \
+            .nid(3) \
+            .nonce(100) \
+            .method("setDelegation") \
+            .params({"delegations": delegations}) \
+            .build()
+        signed_transaction = SignedTransaction(transaction, account)
+        tx_result = self.process_transaction(signed_transaction, self.icon_service)
+        self.assertTrue('status' in tx_result)
+        self.assertEqual(1, tx_result['status'])
+
+    def _get_delegate(self,
+                      account: 'KeyWallet',
+                      accounts: list,
+                      cnt: int,
+                      start_index: int,
+                      init_balance: int,
+                      delegation_value: int):
+        expected_delegations: list = []
+        expected_total_delegation: int = delegation_value * cnt
+        expected_voting_power: int = init_balance - expected_total_delegation
+        if delegation_value > 0:
+            for i in range(start_index, start_index + cnt):
+                delegation: dict = {
+                    "address": str(accounts[i].get_address()),
+                    "value": hex(delegation_value)
+                }
+                expected_delegations.append(delegation)
+        call = CallBuilder().from_(account.get_address()) \
+            .to(self.SYSTEM_ADDRESS) \
+            .method("getDelegation") \
+            .params({"address": account.get_address()}) \
+            .build()
+        response = self.process_call(call, self.icon_service)
+        expected_result = {
+            "delegations": expected_delegations,
+            "totalDelegated": hex(expected_total_delegation),
+            "votingPower": hex(expected_voting_power)
+        }
+        self.assertEqual(expected_result, response)
+
+    def _get_prep_list(self, account: 'KeyWallet', expected_delegated: int):
+        call = CallBuilder().from_(account.get_address()) \
+            .to(self.SYSTEM_ADDRESS) \
+            .method("getPRepList") \
+            .params({}) \
+            .build()
+
+        response = self.process_call(call, self.icon_service)
+        total_delegated = response["totalDelegated"]
+        self.assertEqual(hex(expected_delegated), total_delegated)
+
+    def test_delegate3(self):
+        init_balance: int = 100
+        init_account_count: int = 2
+        init_block_height: int = self._get_block_height()
+
+        accounts: list = []
+        self._make_account_bulk(accounts, balance=init_balance, count=init_account_count)
+
+        # register prep
+        preps = [accounts[0]]
+        self._init_prep(preps)
+
+        # set stake
+        self._set_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance)
+
+        # get stake
+        self._get_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance)
+
+        # set delegate to user1 100%
+        account = accounts[0]
+        start_index: int = 1
+        cnt: int = 1
+        delegation_value: int = init_balance
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate user1 100%
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+        # set delegate to user1 50%
+        account = accounts[0]
+        start_index: int = 1
+        cnt: int = 1
+        delegation_value: int = init_balance // 2
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate user1 50%
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+        # set delegate to user1 0%
+        account = accounts[0]
+        start_index: int = 1
+        cnt: int = 1
+        delegation_value: int = 0
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate user1 0%
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+    def test_delegate4(self):
+        init_balance: int = 100
+        init_account_count: int = 2
+        init_block_height: int = self._get_block_height()
+
+        accounts: list = []
+        self._make_account_bulk(accounts, balance=init_balance, count=init_account_count)
+
+        # register prep
+        self._init_prep(accounts)
+
+        # set stake
+        self._set_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance)
+
+        # get stake
+        self._get_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance)
+
+        # set delegate to user1 100%
+        account = accounts[0]
+        start_index: int = 1
+        cnt: int = 1
+        delegation_value: int = init_balance
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate user1 100%
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+        self._get_prep_list(account, 0)
+
+        # set delegate to user1 50%
+        account = accounts[0]
+        start_index: int = 1
+        cnt: int = 1
+        delegation_value: int = init_balance // 2
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate user1 50%
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+        # set delegate to user1 0%
+        account = accounts[0]
+        start_index: int = 1
+        cnt: int = 1
+        delegation_value: int = 0
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate user1 0%
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+    def test_delegate5(self):
+        init_balance: int = 100
+        init_account_count: int = 3
+        init_block_height: int = self._get_block_height()
+
+        accounts: list = []
+        self._make_account_bulk(accounts, balance=init_balance, count=init_account_count)
+
+        # register prep
+        self._init_prep(accounts)
+
+        # set stake
+        self._set_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance)
+
+        # get stake
+        self._get_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance)
+
+        # set delegate user0 ~ 1
+        account = accounts[0]
+        start_index: int = 0
+        cnt: int = 2
+        delegation_value: int = init_balance // cnt // 2
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate user0 ~ 1
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+        # set delegate user1 ~ 2
+        account = accounts[0]
+        start_index: int = 1
+        cnt: int = 2
+        delegation_value: int = init_balance // cnt // 2
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegation user1 ~ 2
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+    def test_delegate6(self):
+        init_balance: int = 100
+        init_account_count: int = 40
+        init_block_height: int = self._get_block_height()
+
+        accounts: list = []
+        self._make_account_bulk(accounts, balance=init_balance, count=init_account_count)
+
+        # register prep
+        self._init_prep(accounts)
+
+        # set stake
+        self._set_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance)
+
+        # get stake
+        self._get_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance)
+
+        # set delegate user0 ~ 9
+        account = accounts[0]
+        start_index: int = 0
+        cnt: int = 10
+        delegation_value: int = init_balance // cnt // 2
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate user0 ~ 9
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+        # set delegate user10 ~ 19
+        account = accounts[0]
+        start_index: int = 10
+        cnt: int = 10
+        delegation_value: int = init_balance // cnt // 2
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate 10 ~ 19
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+        # set over delegate 20 ~ 29 + 1 (Fail)
+        account = accounts[0]
+        start_index: int = 20
+        cnt: int = 10 + 1
+        delegations: list = []
+        for i in range(start_index, start_index + cnt):
+            delegation: dict = {
+                "address": str(accounts[i].get_address()),
+                "value": hex(delegation_value)
+            }
+            delegations.append(delegation)
+        transaction = CallTransactionBuilder() \
+            .from_(account.get_address()) \
+            .to(self.SYSTEM_ADDRESS) \
+            .step_limit(10_000_000) \
+            .nid(3) \
+            .nonce(100) \
+            .method("setDelegation") \
+            .params({"delegations": delegations}) \
+            .build()
+        signed_transaction = SignedTransaction(transaction, account)
+        tx_result = self.process_transaction(signed_transaction, self.icon_service)
+        self.assertTrue('status' in tx_result)
+        self.assertEqual(0, tx_result['status'])
+
+        # get delegate rollback (10 ~ 19)
+        account = accounts[0]
+        start_index: int = 10
+        cnt: int = 10
+        delegation_value: int = init_balance // cnt // 2
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+        # set delegate all users 0loop
+        account = accounts[0]
+        cnt = 0
+        start_index = 20
+        delegation_value = 0
+        self._set_delegate(account, accounts, cnt, start_index, delegation_value)
+
+        # get delegate all users 0loop
+        account = accounts[0]
+        cnt = 0
+        start_index = 20
+        delegation_value = 0
+        self._get_delegate(account, accounts, cnt, start_index, init_balance, delegation_value)
+
+    def test_delegate7(self):
+        init_balance: int = 100
+        init_account_count: int = 100
+        init_block_height: int = self._get_block_height()
+
+        accounts: list = []
+        self._make_account_bulk(accounts, balance=init_balance, count=init_account_count)
+
+        # register prep
+        self._init_prep(accounts)
+
+        # set stake
+        self._set_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance - x % 100 // 2)
+
+        # get stake
+        self._get_stake_bulk(accounts=accounts,
+                             get_stake=lambda x: init_balance - x % 100 // 2)
 
         # set delegate
         expected_delegation_values: dict = {}
@@ -201,7 +493,7 @@ class TestDelegation(IconIntegrateTestBase):
             self.assertTrue('status' in tx_result)
             self.assertEqual(1, tx_result['status'])
 
-        # get delegation
+        # get delegate
         for i in range(init_account_count):
             total_delegation_value: int = init_balance - i % 100 // 2
             cnt: int = i % 10
@@ -230,12 +522,12 @@ class TestDelegation(IconIntegrateTestBase):
             self.assertEqual(expected_result, response)
 
         # get prep
-        for i in range(init_account_count):
+        for i, account in enumerate(accounts):
             stake_value: int = init_balance - i % 100 // 2
-            call = CallBuilder().from_(accounts[i].get_address()) \
+            call = CallBuilder().from_(account.get_address()) \
                 .to(self.SYSTEM_ADDRESS) \
                 .method("getPRep") \
-                .params({"address": accounts[i].get_address()}) \
+                .params({"address": account.get_address()}) \
                 .build()
             response = self.process_call(call, self.icon_service)
             delegation = response["delegation"]
@@ -258,20 +550,20 @@ class TestDelegation(IconIntegrateTestBase):
                 .method("setDelegation") \
                 .params({"delegations": delegations}) \
                 .build()
-            signed_transaction = SignedTransaction(transaction, accounts[i])
+            signed_transaction = SignedTransaction(transaction, account)
             tx_list.append(signed_transaction)
         tx_results = self.process_transaction_bulk(tx_list, self.icon_service)
         for tx_result in tx_results:
             self.assertTrue('status' in tx_result)
             self.assertEqual(1, tx_result['status'])
 
-        # get delegation
-        for i in range(init_account_count):
+        # get delegate
+        for i, account in enumerate(accounts):
             total_delegation_value: int = init_balance - i % 100 // 2
-            call = CallBuilder().from_(accounts[i].get_address()) \
+            call = CallBuilder().from_(account.get_address()) \
                 .to(self.SYSTEM_ADDRESS) \
                 .method("getDelegation") \
-                .params({"address": accounts[i].get_address()}) \
+                .params({"address": account.get_address()}) \
                 .build()
             response = self.process_call(call, self.icon_service)
             expected_result = {
@@ -282,12 +574,12 @@ class TestDelegation(IconIntegrateTestBase):
             self.assertEqual(expected_result, response)
 
         # get prep
-        for i in range(init_account_count):
+        for i, account in enumerate(accounts):
             stake_value: int = init_balance - i % 100 // 2
-            call = CallBuilder().from_(accounts[i].get_address()) \
+            call = CallBuilder().from_(account.get_address()) \
                 .to(self.SYSTEM_ADDRESS) \
                 .method("getPRep") \
-                .params({"address": accounts[i].get_address()}) \
+                .params({"address": account.get_address()}) \
                 .build()
             response = self.process_call(call, self.icon_service)
             delegation = response["delegation"]
