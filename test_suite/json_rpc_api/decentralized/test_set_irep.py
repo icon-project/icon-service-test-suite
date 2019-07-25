54b31852930368bc5944887d62e8f4fc500bb3ad
inwonkim <inwonkim@icon.foundation>
2019-07-25 18:26:18 +0900

    Add setting irep tests

--- test_suite/json_rpc_api/decentralized/test_set_irep.py
+++ test_suite/json_rpc_api/decentralized/test_set_irep.py
@@ -86,19 +86,91 @@ class TestSetIRep(Base):
         prep2 = self.get_prep(accounts[2])
         self.assertEqual(irep, prep2['irep'])
 
-        # delegate 0 to all preps
-        delegate_info: List[Tuple['Account', int]] = \
-            [(prep, 0) for i, prep in enumerate(accounts[:2])]
+        self.refund_icx(accounts)
+
+    def test_set_irep2(self):
+        if not self.get_main_prep_list():
+            return
+        init_balance: int = (PREP_REGISTER_COST_ICX + 201) * ICX_FACTOR
+        account_count: int = 1
+        accounts: List['Account'] = self.create_accounts(account_count)
+
+        iiss_info1 = self.get_iiss_info()
+        irep = iiss_info1['variable']['irep']
+        self.assertEqual(IISS_MIN_IREP, int(irep, 16))
+
+        # create
+        self.distribute_icx(accounts, init_balance)
+
+        # register prep account0
+        self.register_prep(accounts[:1])
+
+        prep0 = self.get_prep(accounts[0])
+        self.assertEqual(hex(IISS_MIN_IREP), prep0['irep'])
+
         tx_list = []
-        for i, delegate in enumerate(delegate_info):
-            tx = self.create_set_delegation_tx(accounts[i], [delegate])
-            tx_list.append(tx)
+        # try to set irep to IISS_MIN_IREP*0.8 (invalid irep)
+        tx_list.append(self.create_set_governance_variables_tx(accounts[0], IISS_MIN_IREP * 8 // 10))
+        # try to set irep to IISS_MIN_IREP*1.3 (invalid irep)
+        tx_list.append(self.create_set_governance_variables_tx(accounts[0], IISS_MIN_IREP * 13 // 10))
         tx_hashes = self.process_transaction_bulk_without_txresult(tx_list, self.icon_service)
         self.process_confirm_block_tx(self.icon_service)
         tx_results = self.get_txresults(self.icon_service, tx_hashes)
 
         for i, tx_result in enumerate(tx_results):
-            self.assertEqual(tx_result['status'], 1)
-            accounts[i].balance -= tx_result['stepUsed'] * tx_result['stepPrice']
+            self.assertEqual(tx_result['status'], 0)
+            accounts[0].balance -= tx_result['stepUsed'] * tx_result['stepPrice']
+
+        # check irep
+        prep = self.get_prep(accounts[0])
+        self.assertEqual(hex(IISS_MIN_IREP), prep['irep'])
 
         self.refund_icx(accounts)
+
+    def test_set_irep3(self):
+        if not self.get_main_prep_list():
+            return
+        init_balance: int = (PREP_REGISTER_COST_ICX + 201) * ICX_FACTOR
+        account_count: int = 1
+        accounts: List['Account'] = self.create_accounts(account_count)
+
+        iiss_info1 = self.get_iiss_info()
+        irep = iiss_info1['variable']['irep']
+        self.assertEqual(IISS_MIN_IREP, int(irep, 16))
+
+        # create
+        self.distribute_icx(accounts, init_balance)
+
+        # register prep account0
+        self.register_prep(accounts[:1])
+
+        prep0 = self.get_prep(accounts[0])
+        self.assertEqual(hex(IISS_MIN_IREP), prep0['irep'])
+        self._make_blocks_to_end_calculation()
+
+        tx_list = []
+        # try to set irep to IISS_MIN_IREP*1.1(irep set)
+        irep = IISS_MIN_IREP * 11 // 10
+        tx_list.append(self.create_set_governance_variables_tx(accounts[0], irep))
+        # try to set irep to IISS_MIN_IREP*1.1*1.1(will be failed. set irep twice in a term)
+        irep = irep * 11 // 10
+        tx_list.append(self.create_set_governance_variables_tx(accounts[0], irep))
+        tx_hashes = self.process_transaction_bulk_without_txresult(tx_list, self.icon_service)
+        self.process_confirm_block_tx(self.icon_service)
+        tx_results = self.get_txresults(self.icon_service, tx_hashes)
+
+        tx_result = tx_results[0]
+        self.assertEqual(tx_result['status'], 1)
+        accounts[0].balance -= tx_result['stepUsed'] * tx_result['stepPrice']
+
+        tx_result = tx_results[1]
+        self.assertEqual(tx_result['status'], 0)
+        accounts[0].balance -= tx_result['stepUsed'] * tx_result['stepPrice']
+
+        # check irep
+        expected_irep = IISS_MIN_IREP * 11 // 10
+        prep = self.get_prep(accounts[0])
+        self.assertEqual(hex(expected_irep), prep['irep'])
+
+        self.refund_icx(accounts)
+
